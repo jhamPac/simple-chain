e606b4829d94822392b04d1b9189b1f89cdcc689
jhamPac <cloud@kevia.me>
2020-04-20 15:25:22 -0700

    calculating a hash

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"crypto/sha256"
+	"encoding/hex"
+)
+
 // Block in the chain
 type Block struct {
 	Index     int
@@ -11,6 +16,14 @@ type Block struct {
 
 // Blockchain represents a simulated BC
 var Blockchain []Block
+
+func calculateHash(block Block) string {
+	record := string(block.Index) + block.Timestamp + string(block.BPM) + block.PrevHash
+	h := sha256.New()
+	h.Write([]byte(record))
+	hashed := h.Sum(nil)
+	return hex.EncodeToString(hashed)
+}
 
 func main() {
 
